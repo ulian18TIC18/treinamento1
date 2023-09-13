ceaec2bf20736cb6303acc8f791d0260dd740cdd
ulian18TIC18 <ulian18@gmail.com>
2023-09-12 21:34:09 -0300

    Compreendendo o cpp

---
+++ qual_o_seu_nome.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    cout << "Qual o seu nome? " ;
+    string nome;
+    cin >> nome;
+    cout << "Olah, " << nome << " eh um prazer comecar essa jornada!\n";
+
+return 0;
+}
