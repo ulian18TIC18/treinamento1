a2aa7ab50005ccdadb09ca3c9a60b6253bfe0587
ulian18TIC18 <ulian18@gmail.com>
2023-09-13 21:18:15 -0300

    Tamanho de um dado.

---
+++ tamanho_dado.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+cout << "\n\n O tamanho do tipo fundamental de dados eh :\n";
+cout << "----------------------------------------------\n";
+cout << "O sizeof(char) eh:           " << sizeof(char) << "bytes \n" ;
+cout << "O sizeof(short) eh:           " << sizeof(short) << "bytes \n" ;
+cout << "O sizeof(int) eh:           " << sizeof(int) << "bytes \n" ;
+cout << "O sizeof(float) eh:           " << sizeof(float) << "bytes \n" ;
+cout << "O sizeof(long long) eh:           " << sizeof(long long) << "bytes \n" ;
+cout << "O sizeof(double) eh:           " << sizeof(double) << "bytes \n" ;
+cout << "O sizeof(bool)) eh:           " << sizeof(bool) << "bytes \n\n" ;
+
+return 0;
+}
