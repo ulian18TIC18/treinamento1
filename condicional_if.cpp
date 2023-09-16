128b4a2a6267e66debbf4c562cb83580f722451e
ulian18TIC18 <ulian18@gmail.com>
2023-09-16 18:58:10 -0300

    Condicional if

---
+++ condicional_if.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int idade;
+    cout << "Digite sua idade: ";
+    cin >> idade;
+
+    if(idade > 18){
+
+        cout << "Voce eh maior de idade";
+
+    }
+    
+    else{
+
+        cout << "Voce eh menor de idade" ;
+
+    }
+
+return 0;
+}
