83dc3d566bfff5ddc63302fd78b9145473104377
ulian18TIC18 <ulian18@gmail.com>
2023-09-16 22:45:29 -0300

    Contador e armazenamento por vetor

--- contador_vetor_media.cpp
+++ contador_vetor_media.cpp
@@ -7,6 +7,7 @@ int main () {
 
     int notas[10];
     double soma;
+    double media;
 
     for (int i=0; i<10 ; i++){
 
@@ -17,10 +18,10 @@ int main () {
 
     }
 
-    double media = soma/10;
+    media = soma/10;
 
     cout << "A media das notas dos alunos eh igual a: " 
-    << media << "\n" ;
+    << media << "\n" << endl;
 
 return 0;
 }
