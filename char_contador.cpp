084859bb4f048435e53837c340af9a8f3699b6eb
ulian18TIC18 <ulian18@gmail.com>
2023-09-17 00:21:55 -0300

    Quantificando caracteres repetidos A, B, C, D, E e F

---
+++ char_contador.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main (){
+
+    int aQtd = 0, bQtd = 0, cQtd = 0, dQtd = 0, eQtd = 0, fQtd = 0;
+    char nota;
+
+    do{
+    cout << "Digite a nota do aluno: ";
+    cin >> nota;
+
+    switch (nota){
+    
+    case 'A':
+        aQtd ++ ;
+        break;
+    case 'B':
+        bQtd ++ ;
+        break;
+    case 'C':
+        cQtd ++ ;
+        break;
+    case 'D':
+        dQtd ++ ;
+        break;
+    case 'E':
+        eQtd ++ ;
+        break;
+    case 'F':
+        fQtd ++ ;
+        break;
+    
+    default: 
+    
+    cout << "Nota invalida" << endl;    
+        
+    }
+
+
+    cout << "Digite uma nota: " ;
+    cin >> nota;
+    }
+
+    while (nota == 'S' || nota == 's');
+
+        cout << "Quantidade de alunos que obtiveram a nota: \n" << endl;
+
+        cout << "A quantidade de A foi: " << aQtd << endl;
+        cout << "A quantidade de B foi: " << bQtd << endl;
+        cout << "A quantidade de C foi: " << cQtd << endl;
+        cout << "A quantidade de D foi: " << dQtd << endl;
+        cout << "A quantidade de E foi: " << eQtd << endl;
+        cout << "A quantidade de F foi: " << fQtd << "\n" << endl;
+
+
+return 0;
+
+}
