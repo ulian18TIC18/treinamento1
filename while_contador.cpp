465eb56c388c433310c5843dccc33b87b109dbb1
ulian18TIC18 <ulian18@gmail.com>
2023-09-17 19:52:26 -0300

    Contador com while até chegar a 500

---
+++ while_contador.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int contador = 1;
+
+while (contador <= 500)
+{
+    
+    contador = contador*2;
+
+}
+
+cout << contador << "\n" << endl;
+
+
+return 0;
+
+}
