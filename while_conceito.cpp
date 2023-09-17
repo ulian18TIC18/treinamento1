741a80907c36237dba3a5a709e59a17ae9048835
ulian18TIC18 <ulian18@gmail.com>
2023-09-16 23:03:11 -0300

    Conceito de looping infinito

---
+++ while_conceito.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    int numeroPositivo;
+
+    while(numeroPositivo>=0){
+
+        cout << "Digite um numero: \n" << numeroPositivo << endl;
+        cin >> numeroPositivo;
+
+    }
+
+    cout << "O numero eh negativo. Saindo do programa... \n" ;
+
+
+return 0;
+}
