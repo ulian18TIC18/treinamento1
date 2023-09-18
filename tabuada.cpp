26c366f48a70732095c2362f1c2d2a234800d00d
ulian18TIC18 <ulian18@gmail.com>
2023-09-17 21:14:38 -0300

    Tabuada de soma, subtração, multiplicação e divisão

---
+++ tabuada.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+
+    int numero;
+    double divisao;
+
+    cout << "Digite um numero: " ;
+    cin >> numero;
+
+//Operaçoes de soma, subratação e divisão
+for(int i = 0; i < 11; i++){
+
+    cout << numero << " + " << i << " = " << numero + i << endl;
+
+}
+for(int k = 0; k < 11; k++){
+
+    cout << numero << " - " << k << " = " << numero - k << endl;
+
+}
+for(int j = 0; j < 11; j++){
+
+    cout << numero << " * " << j << " = " << numero * j << endl;
+    
+}
+for(int m = 1; m < 11; m++){
+
+    divisao = numero;
+    cout << divisao << " / " << m << " = " << divisao / m << endl;
+
+}
+    cout << "\n" << endl;
+
+return 0;
+}
