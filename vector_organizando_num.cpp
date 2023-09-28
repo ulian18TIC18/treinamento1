a67ea40004e9aacbdec46153d746a2c719a1f304
ulian18TIC18 <ulian18@gmail.com>
2023-09-28 02:29:17 -0300

    Trabalhando com vetores e structs

---
+++ vector_organizando_num.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool test(vector<int> nums)
+{
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < nums.size() - 1; i++){
+
+        if(nums[i + 1 ] != (nums[i] + 1)){
+
+            return false;
+        }
+    }
+    return true;
+}
+
+int main (){
+
+    vector<int> nums = {1, 2 ,5, 7, 4, 3, 6};
+	//vector<int> nums = {1, 2 ,5, 0, 3, 6, 7};
+
+    for (int x : nums)
+    cout << x << " ";
+
+    cout << "Os numeros em sequencia do vetor sao: " << test(nums) << endl;
+
+}
